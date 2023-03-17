7ba02d3dd444cea08ce544e315f3591d93796949
Outelha <talhasaidi115@gmail.com>
2023-03-17 19:30:37 +0100

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,40 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
- * Description : Write a program that prints all possible different
- * combinations of three digits.
- * Numbers must be separated by ,, followed by a space
- * 012, 120, 102, 021, 201, 210 are considered the same
- * combination of the three digits 0, 1 and 2
- * Print only the smallest combination of three digits
+ *
+ * Description: print all possible different
+ *            combinations of two digits.
+ *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-int i, j, k;
-for (i = 48; i <= 57; i++)
+int digit1 = 0;
+int digit2, digit3;
+while (digit1 <= 9)
 {
-for (j = i + 1 ; j <= 57; j++)
+digit2 = 0;
+while (digit2 <= 9)
 {
-for (k = j + 1 ; k <= 57; k++)
+digit3 = 0;
+while (digit3 <= 9)
 {
-putchar(i);
-putchar (j);
-putchar (k);
-if ((i == 56) && (j == 57) && (k == 57))
-break;
+if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
+{
+putchar(digit1 + 48);
+putchar(digit2 + 48);
+putchar(digit3 + 48);
+if (digit1 + digit2 + digit3 != 24)
+{
 putchar(',');
-putchar (' ');
+putchar(' ');
 }
 }
+++digit3;
+}
+++digit2;
+}
+++digit1;
 }
 putchar('\n');
 return (0);
