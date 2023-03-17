594a835fc7e6dc89c50346be789b913d036191b6
Outelha <talhasaidi115@gmail.com>
2023-03-17 19:22:11 +0100

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,10 +2,7 @@
 
 /**
  * main - Entry point
- * Description :program that prints all possible different
- * combinations of two digits.
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Description :program that prints all possible *different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -14,7 +11,7 @@ int i;
 int j;
 for (i = 48; i <= 57; i++)
 {
-for (j = i + 1 ; j <= 57; j++)
+for (j = i + 1; j <= 57; j++)
 {
 putchar(i);
 putchar (j);
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,13 +5,14 @@
  * Description : Write a program that prints all possible different
  * combinations of three digits.
  * Numbers must be separated by ,, followed by a space
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
+ * 012, 120, 102, 021, 201, 210 are considered the same
+ * combination of the three digits 0, 1 and 2
  * Print only the smallest combination of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i,j,k;
+int i, j, k;
 for (i = 48; i <= 57; i++)
 {
 for (j = i + 1 ; j <= 57; j++)
