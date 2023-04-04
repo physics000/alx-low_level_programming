03c73b2a5ef30999786a0cd0885088b2ee24b705
outelha <talhasaidi115@gmail.com>
2023-04-04 00:22:13 +0000

    more pointers and arrays

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -24,7 +24,6 @@ char *_strstr(char *haystack, char *needle)
 		do {
 			if (needle[i + 1] == '\0')
 				return (haystack);
-			
 			i++;
 			} while (haystack[i] == needle[i]);
 	haystack++;
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,7 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int sum1 = 0; 
+	int sum1 = 0;
 	int sum2 = 0;
 
 	for (i = 0; i < size; i++)
