46183fc6026b403956316dc6f94ba762084c28a6
outelha <talhasaidi115@gmail.com>
2023-04-12 23:20:50 +0000

    header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,4 +8,10 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+int len(char *str);
+int num_words(char *str);
 #endif 
