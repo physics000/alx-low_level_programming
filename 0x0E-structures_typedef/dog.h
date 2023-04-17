ddcda318fd7005abc7453b6c59106d9851f1b199
outelha <talhasaidi115@gmail.com>
2023-04-17 22:47:47 +0000

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+void free_dog(dog_t *d);
+#endif
