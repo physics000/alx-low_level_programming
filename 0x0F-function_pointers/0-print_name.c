093528f6ff492a08494abaabfed4ade080285d60
outelha <talhasaidi115@gmail.com>
2023-04-19 22:06:09 +0000

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -11,9 +11,9 @@
  * Return: void
 */
 
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name && f)
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
 
-			f(name);
-	}
+	f(name);
+}
