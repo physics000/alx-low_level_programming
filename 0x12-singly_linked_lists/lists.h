e0bac4e363f755908a97b7d6ac19a9da7e3fa3d8
outelha <talhasaidi115@gmail.com>
2023-04-28 00:00:58 +0000

    lol singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -20,12 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-
-
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 #endif
