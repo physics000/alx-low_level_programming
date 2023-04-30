491a57be8afd23b6dbe29498efeb25eceb4664e6
outelha <talhasaidi115@gmail.com>
2023-05-01 00:50:33 +0100

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,22 +2,24 @@
 
 /**
  * print_listint - Prints all the elements of
- *		a listint_t list recursively.
+ *				a listint_t list recursively.
  *
  * @h: Pointer to the head node of the list.
  *
  * Return: The number of nodes in the list.
  *
+ *	my logic for the code:
+ * i will be using recursion
+ * we give a condition to abase case
+ * then we print the data of the current node
+ * and recursive call to print the remaining nodes
+ * and return the count
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+		if (h == NULL) /*empty list*/
+			return (0);
 
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-	return (count);
+	printf("%d\n", h->n);
+		return (1 + print_listint(h->next));
 }
