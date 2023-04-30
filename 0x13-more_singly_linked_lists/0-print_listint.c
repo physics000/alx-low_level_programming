163e0d1da89688d4cd7616364d2a4219a58df9cd
outelha <talhasaidi115@gmail.com>
2023-05-01 00:23:39 +0100

    more singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -17,8 +17,8 @@
  */
 size_t print_listint(const listint_t *h)
 {
-		if (h == NULL) /*empty list*/
-			return (0);
+	if (h == NULL) /*empty list*/
+		return (0);
 
 	printf("%d\n", h->n);
 		return (1 + print_listint(h->next));
