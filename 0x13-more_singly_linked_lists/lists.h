6e852a2891025645912639f3efb399049623d0fa
outelha <talhasaidi115@gmail.com>
2023-05-02 18:33:26 +0100

    we fall, we break, we fail, but then we rise, we heal , WE OVERCOME

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -38,6 +38,6 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 void free_listp(listp_t **head);
-
+size_t looped_listint_len(const listint_t *head);
 #endif
 
