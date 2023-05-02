61a94c018dc31e58e794cefa3ffe73b7912c0633
outelha <talhasaidi115@gmail.com>
2023-05-02 23:10:07 +0100

    we fall, we break, we fail, but then we rise, we heal , WE OVERCOME

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,8 @@
 #define LISTS_H
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,10 +17,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-#include <stdlib.h>
-#include <string.h>
-
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
